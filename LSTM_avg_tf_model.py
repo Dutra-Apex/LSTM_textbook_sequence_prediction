a3e32bd497907198de16f2c2ada0f38de0bd6262
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-03-07 17:39:25 -0500

    Update LSTM_avg_tf_model.py

--- LSTM_avg_tf_model.py
+++ LSTM_avg_tf_model.py
@@ -16,3 +16,18 @@ model.add(LSTM((1), batch_input_shape=(None,2,1), return_sequences=True))
 model.add(LSTM((1), return_sequences=False))
 model.compile(loss = 'mean_absolute_error', optimizer='adam')
 model.summary()
+
+# Fits the x_train and y_train data into the model
+history = model.fit(x_train, y_train, batch_size = 2, epochs= 500, validation_data=(x_test,y_test))
+
+results = model.predict(x_test)
+
+# Plots the predicted results vs the actual results
+plt.figure(figsize=(10,6))
+plt.title('Predicted tf-idf (green) & Actual tf-idf (red) - Good sequence')
+plt.xlabel('Section')
+plt.ylabel('Tf-idf value')
+plt.scatter(range(len(results)), results, c='g')
+plt.scatter(range(len(y_test)), y_test, c='r')
+plt.show()
+
