18aca2368ca7a2ef679ae9622e647fbbec9163c4
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-04-25 15:46:41 -0400

    Update and rename Sorted_Tf_Predictions to Sorted_Tf_Predictions.py

--- Sorted_Tf_Predictions.py
+++ Sorted_Tf_Predictions.py
@@ -8,3 +8,15 @@ from keras.models import Sequential
 from keras.layers import Dense, LSTM
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
+
+#Creates array with all the tf-idf values of the good_seq
+good_seq_all_tf = []
+placeholder = []
+
+for i in range(0, len(good_seq)):
+  for j in good_seq[i]:
+    placeholder.append(M_OS[j])
+  good_seq_all_tf.append(placeholder)
+  placeholder = []
+
+print(len(good_seq_all_tf[0]))
